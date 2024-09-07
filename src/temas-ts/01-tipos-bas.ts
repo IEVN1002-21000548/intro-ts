7fa9af1124d0e0d978bc69f5287999fa5174f059
Ricardo Alexis Hernandez Rodriguez <21000548@alumnos.utleon.edu.mx>
2024-09-07 10:08:25 -0600

    Primer commit intro-ts

---
+++ src/temas-ts/01-tipos-bas.ts
@@ -0,0 +1,17 @@
+
+var num1=23;
+let num2="Juan";
+const num3=34;
+
+const sum = num1 + num2 + num3;
+console.log(`La suma es: ${sum}`)
+
+let nombre:string = "Juan";
+let edad:number = 23;
+let activo:boolean = true;
+
+//edad = ""
+
+let matricula:number|string;
+matricula = 123456;
+matricula='1234asdas'
