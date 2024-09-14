c3dc5d7f69b6ecc7c8998a4a0dc150b839cce3e0
Ricardo Alexis Hernandez Rodriguez <21000548@alumnos.utleon.edu.mx>
2024-09-14 09:57:43 -0600

    Actividad Clase 1

--- src/temas-ts/distanciaEntreDosPuntos.ts
+++ src/temas-ts/distanciaEntreDosPuntos.ts
@@ -1,19 +1,23 @@
-class Distancia{
-    Punto_x1:number;
-    Punto_x2:number;
-    Punto_y1:number;
-    Punto_y2:number;
-    constructor (Punto_x1:number,Punto_x2:number,Punto_y1:number,Punto_y2:number){
-        this.Punto_x1=Punto_x1;
-        this.Punto_y1=Punto_y1;
-        this.Punto_x2=Punto_x2;
-        this.Punto_y2=Punto_y2;
+export class Distancia {
+    Punto_x1: number;
+    Punto_x2: number;
+    Punto_y1: number;
+    Punto_y2: number;
+
+    constructor(Punto_x1: number, Punto_x2: number, Punto_y1: number, Punto_y2: number) {
+        this.Punto_x1 = Punto_x1;
+        this.Punto_y1 = Punto_y1;
+        this.Punto_x2 = Punto_x2;
+        this.Punto_y2 = Punto_y2;
     }
-    imprimir(){
-        console.log(`La distancia entre los puntos es : ${Math.sqrt((this.Punto_x2 - this.Punto_x1)**2 + (this.Punto_y2 - this.Punto_y1)**2)}`)
+
+    calcularDistancia(): number {
+        const disTotal = Math.sqrt((this.Punto_x2 - this.Punto_x1) ** 2 + (this.Punto_y2 - this.Punto_y1) ** 2);
+        console.log(`La distancia entre los puntos es : ${disTotal}`)
+        return disTotal;
     }
 }
 
 let dis:Distancia;
-dis = new Distancia(2,5,3,1)
-dis.imprimir()
+dis = new Distancia(-4,2,-3,5)
+dis.calcularDistancia()
---
+++ src/temas-ts/triangulo.ts
@@ -0,0 +1,38 @@
+import { Distancia } from "./distanciaEntreDosPuntos";
+
+class Triangulo extends Distancia {
+    Punto_x3: number;
+    Punto_y3: number;
+
+    constructor(Punto_x1: number, Punto_y1: number, Punto_x2: number, Punto_y2: number, Punto_x3: number, Punto_y3: number) {
+        super(Punto_x1, Punto_x2, Punto_y1, Punto_y2); 
+        this.Punto_x3 = Punto_x3;
+        this.Punto_y3 = Punto_y3;
+    }
+
+    esTriangulo(): boolean {
+        const d1 = this.calcularDistancia();
+
+        const distanciaPunto2a3 = new Distancia(this.Punto_x2, this.Punto_x3, this.Punto_y2, this.Punto_y3);
+        const d2 = distanciaPunto2a3.calcularDistancia();
+
+        const distanciaPunto3a1 = new Distancia(this.Punto_x3, this.Punto_x1, this.Punto_y3, this.Punto_y1);
+        const d3 = distanciaPunto3a1.calcularDistancia(); 
+
+        return d1 + d2 > d3 && d2 + d3 > d1 && d3 + d1 > d2;
+    }
+
+    imprimir() {
+        if (this.esTriangulo()) {
+            console.log("Los puntos SI forman un triángulo.");
+        } else {
+            console.log("Los puntos NO forman un triángulo.");
+        }
+    }
+}
+//3, 2, 4, 6, 7, 1
+let triangulo1 = new Triangulo(0, 0, 1, 1, 2, 2);
+triangulo1.imprimir(); 
+
+let triangulo2 = new Triangulo(0, 0, 1, 1, 2, 2);
+triangulo2.imprimir(); 
